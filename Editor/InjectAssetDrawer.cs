429c07f6ff57405d1526f11a54e394f6cac0b597
Pekka Heikkinen <pekka.tilit@aijai.org>
2016-07-28 10:17:51 +0300

    Initial commit

---
+++ Editor/InjectAssetDrawer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(InjectAssetAttribute))]
+public class InjectAssetDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty prop, GUIContent label)
+    {
+        var attr = attribute as InjectAssetAttribute;
+        if (prop.objectReferenceValue == null)
+        {
+            var allAssets = AssetDatabase.FindAssets("t:" + attr.assetType.Name);
+            if (allAssets.Length != 0)
+            {
+                Object t = AssetDatabase.LoadAssetAtPath<Object>(
+                    AssetDatabase.GUIDToAssetPath(allAssets[0])
+                    );
+                prop.objectReferenceValue = t;
+            }
+        }
+        prop.objectReferenceValue = EditorGUI.ObjectField(position,
+            label,
+            prop.objectReferenceValue,
+            attr.assetType,
+            false);
+    }
+}
---
+++ InjectAssetAttribute.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class InjectAssetAttribute : PropertyAttribute
+{
+    public System.Type assetType;
+
+    public InjectAssetAttribute(System.Type type)
+    {
+        assetType = type;
+    }
+}
